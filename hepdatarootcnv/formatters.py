fdde3676c8c8ae1314f37f38bfc35a36586e5e6f
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-09-23 14:35:37 +0100

    simplification

--- hepdatarootcnv/formatters.py
+++ hepdatarootcnv/formatters.py
@@ -1,17 +1,21 @@
-def standard_format(bin_info_dep,**kwargs):
-  v = bin_info_dep.values()[0]
+# conventions for histogram formatters
+# bin_info_dep is a dictionary of {'histoname':{'value':v,'error_plus':plus_error,'error_minus':minus_error}}
+# indep_info is a dictionary {'low':low_edge,'width':bin_width}
+
+def standard_format(dep_info,**kwargs):
+  v = dep_info.values()[0]
   error_config = kwargs.get('error_config',None)
   error = {}
   if error_config == 'asymmetric':
-    error = {'asymerror':{'minus':-v['error']['minus'],'plus':v['error']['plus']},'label':kwargs['label']}
+    error = {'asymerror':{'minus':-v['error_minus'],'plus':v['error_plus']},'label':kwargs['label']}
   if error_config == 'symmetric':
-    error = {'symerror':(v['error']['plus']+v['error']['minus'])/2,'label':kwargs['label']}
+    error = {'symerror':(v['error_plus']+v['error_minus'])/2,'label':kwargs['label']}
   data = {'value':v['value']}
   if error: data['errors'] = [error]
   return data
 
-def nominal_with_variations_formatter(bin_info_dep,**kwargs):
-  nom,up,down = [bin_info_dep[x]['value'] for x in ['nominal','up','down']]
+def nominal_with_variations_formatter(dep_info,**kwargs):
+  nom,up,down = [dep_info[x]['value'] for x in ['nominal','up','down']]
   return {'value':nom,'errors':[
     {'asymerror':{'minus':down-nom,'plus':up-nom},
      'label':kwargs['label']}
