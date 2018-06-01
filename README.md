e1c66870c0d555e9d23b5dd8b3f1f158473f43f5
Louie <louie.dartmoor.corpe@cern.ch>
2018-06-01 15:50:45 +0200

    Additional formatter to handle multiple error sources, added a little extra documentatiom

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
+# installation 
+e.g on lxplus machine:
+```
+cd myWorkingDir 
+virtualenv venv
+source virtualenv/bin/activate
+export PYTHONPATH=$PWD/lib/python2.7/site-packages
+export PYTHONPATH=$PWD/local/lib64/python2.7/site-packages:$PYTHONPATH
+export PATH="$PWD/bin:$PATH"
+pip install -e . --prefix $PWD
+```
 # hepdata-rootcnv
 
 Flexible ROOT conversion. Example:
+```
+./bin/hepdata-rootcnv example.yaml 
+```
 
-    hepdata-rootcnv example.yaml 
+# write your own formatter
+Some examples are given in `example.yaml`, but may not suit the structure of your data exactly.
+You can write your own formatter to suit your needs. See `hepdatarootcnv/formatters.py` for some examples, and add yours there. 
--- hepdatarootcnv/formatters.py
+++ hepdatarootcnv/formatters.py
@@ -20,6 +20,54 @@ def nominal_with_variations_formatter(dep_info,**kwargs):
     {'asymerror':{'minus':down-nom,'plus':up-nom},
      'label':kwargs['label']}
    ]}
+
+def nominal_with_multiple_variations_formatter(dep_info,**kwargs):
+  #nom,sym = [dep_info[x]['value'] for x in ['nominal','sym']]
+  #return {'value':nom,'errors':[
+  #  {'symerror':sym*nom*0.01,
+  #   'label':kwargs['label']}
+  # ]}
+  res={}
+  nom = dep_info['nominal']['value']
+  res['value']=nom
+  res['errors']=[]
+  for var in dep_info:
+    if var=='nominal': continue
+    if "_dn" in var and var.replace("_dn","_up") in dep_info: continue
+    elif "_up" in var and var.replace("_up","_dn") in dep_info: 
+      sym_or_asym='asymerror'
+      sys_or_stat='sys' if 'sys' in var else 'stat'
+      error_label=var.replace("_up","").replace(sys_or_stat+"_",sys_or_stat+",")
+      error_val_up=dep_info[var]['value']
+      error_val_dn=dep_info[var.replace("_up","_dn")]['value']
+      error_val={'plus':error_val_up, 'minus':error_val_dn}
+      error_type=kwargs['error_type']
+      if error_type=='relative':
+        error_val['plus']*=nom*kwargs['error_multiplier']
+        error_val['minus']*=nom*kwargs['error_multiplier']
+      if error_type=='absolute':
+        error_val['plus']+=nom
+        error_val['minus']+=nom
+    else:
+      sym_or_asym='symerror'
+      sys_or_stat='sys' if 'sys' in var else 'stat'
+      error_label=var.replace(sys_or_stat+"_",sys_or_stat+",")
+      error_val=dep_info[var]['value']
+      error_type=kwargs['error_type']
+      if error_type=='relative':
+        error_val*=nom*kwargs['error_multiplier']
+      if error_type=='absolute':
+        error_val+=nom
+
+    res['errors'].append({sym_or_asym:error_val,
+                          'label':error_label})
+
+  return res
+  #up,down = [dep_info[x]['value'] for x in ['nominal',']]
+  #return {'value':nom,'errors':[
+  #  {'asymerror':{'minus':down-nom,'plus':up-nom},
+  #   'label':kwargs['label']}
+  # ]}
    
 def bin_format(indep_info,**kwargs):
   style = kwargs.get('style',None)
